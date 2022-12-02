cb6af18b8aaf0efa0743cfe3b2007ae53c7f9b93
tayjouonn <113881849+tayjouonn@users.noreply.github.com>
2022-12-02 11:38:51 +0800

    Add files via upload

---
+++ js/contactus.js
@@ -0,0 +1,14 @@
+
+function myFunction1() {
+    location.href = "https://www.instagram.com/salon_modish/";
+}
+
+
+function myFunction2() {
+    location.href = "https://www.facebook.com/modishsalon18";
+}
+
+
+function myFunction3() {
+    location.href = "https://api.whatsapp.com/send/?phone=%2B600162322520&text&type=phone_number&app_absent=0";
+}
